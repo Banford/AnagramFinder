0f95f1ae3275db2d94d78df419e95448fb1cc6a8
Nick Banford <nick.banford@purplebricks.com>
2021-08-05 19:52:18 +0100

    Initial commit.

---
+++ Banford.AnagramFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Banford.AnagramFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
