975e5482028d2af74bef90ff06ec3d7e1629e017
Nick Banford <nick.banford@purplebricks.com>
2021-08-05 19:54:22 +0100

    Adds an empty xunit test project.

---
+++ Banford.AnagramFinder.Tests/AnagramFinderTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Banford.AnagramFinder.Tests
+{
+    public class AnagramFinderTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // TODO
+        }
+    }
+}
