b9b2a9ae9fae47767c37835e8b0194410aff885f
Nick Banford <nick.banford@purplebricks.com>
2021-08-05 21:40:10 +0100

    Adds logging using Serilog along with some error handling.

--- Banford.AnagramFinder/Program.cs
+++ Banford.AnagramFinder/Program.cs
@@ -1,12 +1,49 @@
-﻿namespace Banford.AnagramFinder
+﻿using System;
+using System.IO;
+using Serilog;
+
+namespace Banford.AnagramFinder
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            
+            Log.Information("Starting AnagramFinder...");
+            
+            if (args.Length != 1)
+            {
+                Log.Information("Wrong number of arguments provided.");
+                Console.Write("Please provide a single file path as the only argument to this program.");
+                return;
+            }
+
             var fileName = args[0];
 
-            AnagramFileProcessor.Process(fileName);
+            if (!File.Exists(fileName))
+            {
+                Log.Information("File did not exist.");
+                Console.WriteLine("The file you tried to process does not exist. Please check and try again.");
+                return;
+            }
+
+            try
+            {
+                Log.Debug("Processing {A}", fileName);
+                AnagramFileProcessor.Process(fileName);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "There was an error processing the input file.");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,40 @@
+﻿# Anagram Finder
+
+This console app will take a single file path to a file containing a list of words. It will read the file line by line and process groups of words of the same length in a batch. Processing each batch of words will reveal the groups of anagrams in the batch of words.
+
+## Prerequisites
+
+This project was built using .Net SDK version 5.0.302.
+
+## Building the Project
+
+From the solution directory run:
+
+`dotnet build`
+
+This will build both the test project and main project.
+
+## Testing the Project
+
+Tests are written using xUnit and can be run from the dotnet command line, or via an IDE such as Rider or Visual Studio.
+
+`dotnet test` - Run tests from the command line.
+
+## Running the Project
+
+From the solution directory run:
+
+Example 1:
+
+`dotnet run --project .\Banford.Anagrams\Banford.Anagrams.csproj ".\Data\example1.txt"`
+
+Example 2:
+
+`dotnet run --project .\Banford.Anagrams\Banford.Anagrams.csproj ".\Data\example2.txt"`
+
+## Assumptions
+
+* Assumption is that all input will be lowercase words. Code could be adapted to support mixed case.
+* If the word is the same word, we wouldn't count this as an anagram, as it is a duplication.
+* Only output groups of anagrams that have at least one matching word (2 versions of the anagram minimum).
+* The input will always have a new word on each line without gaps.
