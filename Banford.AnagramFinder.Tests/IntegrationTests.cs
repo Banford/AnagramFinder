7a23aef86c0348d3f68f6eb0621b93ad49286d2d
Nick Banford <nick.banford@purplebricks.com>
2021-08-05 21:59:23 +0100

    Adds an initial integration test.

---
+++ Banford.AnagramFinder.Tests/IntegrationTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace Banford.AnagramFinder.Tests
+{
+    public class IntegrationTests
+    {
+        protected Process StartApplication()
+        {
+            var programLocation = Assembly.GetAssembly(typeof(AnagramFinder))?.Location;
+            var executablePath = Path.GetDirectoryName(programLocation);
+
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = executablePath + @"\Banford.AnagramFinder.exe",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true
+            };
+
+            return Process.Start(processStartInfo);
+        }
+        
+        [Fact]
+        public void ProgramShouldRun()
+        {
+            // Arrange
+            var process = StartApplication();
+
+            // Act
+            var output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            // Assert
+            Assert.Contains("Starting AnagramFinder...", output);
+        }
+    }
+}
