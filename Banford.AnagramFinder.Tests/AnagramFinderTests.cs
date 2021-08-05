99c97a51ef25b0f51b1f3bd5de7bae3ede6ac68d
Nick Banford <nick.banford@purplebricks.com>
2021-08-05 19:59:22 +0100

    Adds an initial unit test, hard coded to pass first case.

--- Banford.AnagramFinder.Tests/AnagramFinderTests.cs
+++ Banford.AnagramFinder.Tests/AnagramFinderTests.cs
@@ -1,14 +1,27 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
+using Banford.AnagramFinder;
 
 namespace Banford.AnagramFinder.Tests
 {
     public class AnagramFinderTests
     {
         [Fact]
-        public void Test1()
+        public void FindAnagrams_ShouldReturnAnagramsInListOfWords()
         {
-            // TODO
+            // Arrange
+            var input = new[] {"abc", "cba"};
+
+            // Act
+            var finder = new AnagramFinder();
+            var result = finder.FindAnagrams(input);
+
+            // Assert
+            var expected = new List<string> {"abc", "cba"};
+
+            Assert.Equal(expected, result.First().Value);
         }
     }
 }
---
+++ Banford.AnagramFinder/AnagramFinder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Banford.AnagramFinder
+{
+    public class AnagramFinder
+    {
+        public Dictionary<string, List<string>> FindAnagrams(string[] input)
+        {
+            // Hard coded to pass first test...
+            return new Dictionary<string, List<string>>
+            {
+                {"abc", new List<string> {"abc", "cba"}}
+            };
+        }
+    }
+}
